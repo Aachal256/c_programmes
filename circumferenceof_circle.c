7a2e250d12ae1c04a1c843adb22bbda447bca975
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 15:28:53 +0530

    Add files via upload

---
+++ circumferenceof_circle.c
@@ -0,0 +1,14 @@
+//  WAP to calculate circumference of a circle.
+
+#include<stdio.h>
+int main()
+{
+    int r;
+    float c;
+    printf("Enter a number:");
+    scanf("%d",&r);
+    c=2*3.14*r;
+    printf("circumference of a circle is %f",c);
+    return 0;
+
+}
