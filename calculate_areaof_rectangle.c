90617d6b92f82c0c33e2223c6a172a355b91f5f4
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 14:54:48 +0530

    Add files via upload

---
+++ calculate_areaof_rectangle.c
@@ -0,0 +1,13 @@
+//WAP to calculate area of a rectangle. input appropriate data from the user.
+
+#include<stdio.h>
+int main()
+{
+    int a,l,w;
+    printf("Enetr lenth and width:");
+    scanf("%d%d",&l,&w);
+    a=l*w;
+    printf("area of rectangle is %d",a);
+    return 0;
+
+}
