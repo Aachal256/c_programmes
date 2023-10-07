6673a42052ee2c1af478bbf3a10d17fd0780e761
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 14:35:39 +0530

    Add files via upload

---
+++ single.c
@@ -0,0 +1,8 @@
+// WAP to print "Teacher's Day" on the screen(remmember to print in double and single quotes.)
+
+#include<stdio.h>
+int main()
+{
+    printf("\"Teacher's Day\"");
+    return 0;
+}
