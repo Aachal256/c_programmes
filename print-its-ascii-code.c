2e636b0a724b0bac7f3630dc594b3204f9d27b3d
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 17:08:17 +0530

    Add files via upload

---
+++ print-its-ascii-code.c
@@ -0,0 +1,11 @@
+//WAP to input a character from the user and print its ascii code.
+
+#include<stdio.h>
+int main()
+{
+    char k;
+    printf("Enter a character:");
+    scanf("%c",&k);
+    printf("%c = %d",k,k);
+    return 0;
+}
