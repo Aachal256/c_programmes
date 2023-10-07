d344638f1eb4a9cc44a31930153e2fcbeec08307
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 14:39:22 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+//WAP to calculate sum of two integers. numbers are taken from  user through keyboard.
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum is %d",c);
+    return 0;
+}
