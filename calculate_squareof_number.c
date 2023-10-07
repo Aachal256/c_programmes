ee3e1541ae789d89c9d3b4997b5f37ce9d17e339
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-07 14:43:55 +0530

    Add files via upload

---
+++ calculate_squareof_number.c
@@ -0,0 +1,14 @@
+//WAP to calculate square of a given number. number is entered by the user.
+
+#include<stdio.h>
+int main()
+{
+    int a,k;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    k=a*a;
+    printf("square is %d",k);
+    return 0;
+    
+
+}
