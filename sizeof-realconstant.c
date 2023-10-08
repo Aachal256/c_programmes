00b0fd6db8a349c67627cb00c0ebdc9501d4d07e
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-08 13:14:22 +0530

    Add files via upload

---
+++ sizeof-realconstant.c
@@ -0,0 +1,10 @@
+// WAP to calculate size of a real constant.
+
+#include<stdio.h>
+int main()
+{
+    printf("size = %d",sizeof(4.5));
+    return 0;
+}
+
+// In this program real constant by default double thatsway its size is 8.
