b14ce3b4f57ccb2f24383d159f411f458a7f9961
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-08 13:10:46 +0530

    Add files via upload

---
+++ sizeof-characterconstant.c
@@ -0,0 +1,21 @@
+// WAP to calculate size of a character constant.
+
+/*  It have two method to find size given below two methods.
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    x=sizeof('A');
+    printf("%d",x);
+    return 0;
+}
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("size= %d",sizeof('A'));
+    return 0;
+}
