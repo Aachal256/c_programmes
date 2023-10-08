69d112fd292e10506a0f1311ac2d32c7328a30f8
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-08 15:40:51 +0530

    Add files via upload

---
+++ print-sizeofdata-type.c
@@ -0,0 +1,16 @@
+// WAp to print size of an int ,a float,a char and a double type variable....
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("\nsize of int = %d",sizeof(int));
+    printf("\nsize of float = %d",sizeof(float));
+    printf("\nsize of char = %d",sizeof(char));
+    printf("\nsize of double = %d",sizeof(double));
+    return 0;
+
+}
