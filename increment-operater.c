ae0d986c755d66a807e98a4cd02ce483870340f3
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-08 13:21:37 +0530

    Add files via upload

---
+++ increment-operater.c
@@ -0,0 +1,10 @@
+// WAP with one char type variable. assign 'A' in the variable. now change the value of variable from 'A' to 'B' using increment operator.
+
+#include<stdio.h>
+int main()
+{
+    char ch='A';
+    ch++;
+    printf("increase value = %c ",ch);
+    return 0;
+}
