685c8bbad18c8c2927b0b8af76deddfece8d9eb5
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:15:53 +0530

    Add files via upload

---
+++ odd-tennaturalnumbers.c
@@ -0,0 +1,29 @@
+//WAP to print the first  10 odd natural numbers  ....
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=19)
+    {
+    printf("\n%d",i);
+    i=i+2;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
+*/
