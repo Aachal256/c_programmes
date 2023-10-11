44d0ee8cb45ab03a7e4c412e9c077d6fac445d84
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:06:50 +0530

    Add files via upload

---
+++ reverseorder-printtennumber.c
@@ -0,0 +1,30 @@
+//WAP to print the first 10 natural numbers in reverse order...
+
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    while(i>=1)
+    {
+    printf("\n%d",i);
+    i--;
+    }
+    return 0;
+}
+
+
+/*
+o/p=)
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+*/
