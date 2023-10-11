5cc6308b277cb418d2ef223cc2aacdea6b2bd084
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:37:08 +0530

    Add files via upload

---
+++ evenreverse-tennaturalnumber.c
@@ -0,0 +1,29 @@
+//WAP to print the first  10 even natural numbers in reverse order...
+
+#include<stdio.h>
+int main()
+{
+    int i=20;
+    while(i>=2)
+    {
+    printf("\n%d",i);
+    i=i-2;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+
+20
+18
+16
+14
+12
+10
+8
+6
+4
+2
+
+*/
