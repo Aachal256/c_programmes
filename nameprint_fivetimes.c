396ecc21b8ba4197db24420efe7b71e085cf45de
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 19:54:02 +0530

    Add files via upload

---
+++ nameprint_fivetimes.c
@@ -0,0 +1,24 @@
+//WAP to print AACHAL 5 times on the screen..
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+    printf("\nAACHAL");
+    i++;
+    }
+    return 0;
+}
+
+
+/*
+o/p=  
+
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+*/
