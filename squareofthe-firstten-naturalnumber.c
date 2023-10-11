0977014fa2ac697bbc47c2a0f12bb01eadb35cb3
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:44:31 +0530

    Add files via upload

---
+++ squareofthe-firstten-naturalnumber.c
@@ -0,0 +1,29 @@
+// WAP  to print squares of the first 10 natural numbers...
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("\n%d",i*i);
+    i++;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
+
+*/
