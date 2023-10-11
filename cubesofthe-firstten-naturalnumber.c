48cbaafefd8a2bd7b1760823083a7878348b1ba4
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:49:34 +0530

    Add files via upload

---
+++ cubesofthe-firstten-naturalnumber.c
@@ -0,0 +1,28 @@
+// WAP  to print cubes of the first 10 natural numbers...
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("\n%d",i*i*i);
+    i++;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+1
+8
+27
+64
+125
+216
+343
+512
+729
+1000
+
+*/
