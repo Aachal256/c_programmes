a1210a333267ab79e25212745365a3fc04e55eb6
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:24:28 +0530

    Add files via upload

---
+++ reverse-oddtennaturalnumber.c
@@ -0,0 +1,29 @@
+//WAP to print the first  10 odd natural numbers  in reverse order....
+
+#include<stdio.h>
+int main()
+{
+    int i=19;
+    while(i>=1)
+    {
+    printf("\n%d",i);
+    i=i-2;
+    }
+    return 0;
+}
+
+
+/*
+o/p=)
+19
+17
+15
+13
+11
+9
+7
+5
+3
+1
+
+*/
