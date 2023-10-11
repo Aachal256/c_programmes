f060669c815cde8ad8234c319362fa11e5fcb8e8
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 19:57:51 +0530

    Add files via upload

---
+++ first_ten-naturalnoprint.c
@@ -0,0 +1,29 @@
+//WAP to print the first 10 natural numbers...
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("\n%d",i);
+    i++;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*/
