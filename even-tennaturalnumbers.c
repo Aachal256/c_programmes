293d21a75acad0166ad93e9be3178c67d3aed1cb
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-11 20:32:56 +0530

    Add files via upload

---
+++ even-tennaturalnumbers.c
@@ -0,0 +1,28 @@
+//WAP to print the first  10 even natural numbers ...
+
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    while(i<=20)
+    {
+    printf("\n%d",i);
+    i=i+2;
+    }
+    return 0;
+}
+
+/*
+o/p=)
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+
+*/
