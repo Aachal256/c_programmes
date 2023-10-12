745c1c00175bf36537ed14acc3152e78eda4f0ec
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-12 11:24:23 +0530

    Add files via upload

---
+++ ntimes-nameprint.c
@@ -0,0 +1,29 @@
+// WAP to print AACHAL N times on the screen...
+
+#include<stdio.h>
+int main()
+{
+    int i=1,a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    while(i<=a)
+    {
+    printf("\nAACHAL");
+    i++;
+    }
+    return 0;
+
+}
+
+/*
+i/p=  Enter a number:8
+o/p=)
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+AACHAL
+*/
