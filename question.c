01c9ef3df36a090715783ef0c157b261265d782e
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-10-15 13:16:40 +0530

    Add files via upload

---
+++ question.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=0;i<=3;i++)
+    {
+        j=i*2+1;
+        k=j*3-2;
+    }
+    printf(" %d %d %d",i,j,k);
+    
+    return 0;
+}
+
+/*
+o/p=)  4 7 19
+*/
