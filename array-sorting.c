9bf3bce4f436e2834c6fc63bead0e084acc04239
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-11-06 23:13:00 +0530

    Add files via upload

---
+++ array-sorting.c
@@ -0,0 +1,38 @@
+// WRITE A PROGRAM TO SORT ELEMENT OF AN ARRAY OF SIZE 10..TAKE ARRAY VALUES FROM THE USER..
+
+#include<stdio.h>
+int main()
+{
+    int a[5],i,j,temp;
+    printf("Enter 5 number:");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++) //i=0
+    {
+        for(j=i+1;j<5;j++) //j=1
+        {
+            if(a[i]>a[j])   
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+
+            }
+        }
+
+    }
+    printf("Sorting Element in ascending order = ");
+    for(i=0;i<5;i++)
+    {
+        printf(" %d",a[i]);
+    }
+    return 0;
+}
+
+/*
+i/p--   Enter a number:4 2 3 5 1
+ o/p--  Sorting Element in ascending order  1 2 3 4 5
+*/
+// By default array sort in assending order..
