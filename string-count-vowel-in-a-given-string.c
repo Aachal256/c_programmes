a11cfcf30fd856f781f3a3194c2fb4f233a3dbd9
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-11-11 23:48:50 +0530

    Add files via upload

---
+++ string-count-vowel-in-a-given-string.c
@@ -0,0 +1,28 @@
+// WRITE A PROGRAM TO COUNT VOWEL IN A GIVEN STRING.
+#include<stdio.h>
+int main()
+{
+    char str[50],V[]="aeiouAEIOU";
+    int i,k,count=0;
+    printf("Enter a string");
+    fgets(str,50,stdin);
+    for(i=0;str[i];i++);
+    str[i-1]='\0';
+    for(i=0;str[i];i++)
+        for(k=0;V[k];k++)
+            if(str[i]==V[k])
+                count++;
+    printf("Vowel Count is %d",count);   
+    return 0;  
+}
+
+/*
+i/p--  Enter a string mysirg education service
+o/p--  Vowel Count is 9
+*/
+ 
+ 
+ 
+
+ 
+ 
